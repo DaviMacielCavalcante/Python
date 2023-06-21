8bb968bd4118625c18a934f6cdbef52b92341475
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2023-06-21 17:14:52 -0300

    Seção 2 - Programação Básica em Python Finalizada

---
+++ Estrutura de Dados e Algoritmos/Programação Básica/modulo_exercicios6.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 20 11:05:40 2023
+
+@author: Davi
+"""
+
+def imprime(texto):
+    return print(texto)
+
+def ler_float(texto_numero):
+    numero = float(texto_numero)
+    return numero
---
+++ Estrutura de Dados e Algoritmos/Programação Básica/utilitarios.py
@@ -0,0 +1,16 @@
+def soma(a,b,c):
+    somatorio = a + b + c
+    return somatorio
+
+def mult(a,b,c):
+    produto = a * b * c
+    return produto
+
+def isPalindromo(string):
+    if string == string[::-1]:
+        return True
+    else:
+        return False
+
+def divisao(a, b):
+    return a / b
